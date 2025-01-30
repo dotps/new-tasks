0de341c7fad2bb5fc0a93d2f22497fa265b22de5
Stas Petrov <petrovstas@mail.ru>
2025-01-30 18:47:41 +0500

    feat: add router

--- src/App.ts
+++ src/App.ts
@@ -1,17 +1,26 @@
 import express, {Application} from 'express'
+import {UserRouter} from "./Routes/UserRouter"
+import {IRouter} from "./Routes/IRouter"
 
 export class App {
     private app: Application
+    private userRouter: IRouter
 
     constructor() {
         this.app = express()
         this.app.use(express.json())
+        this.userRouter = new UserRouter()
+        this.initRoutes()
     }
 
     public start(port: number): void {
         this.app.listen(port, () => {
             console.log(`Сервер запущен, порт: ${port}`)
         })
+    }
+
+    private initRoutes(): void {
+        this.app.use('/users', this.userRouter.getRouter())
     }
 }
 
---
+++ src/Routes/UserRouter.ts
@@ -0,0 +1,24 @@
+import { Router, Request, Response } from 'express'
+import {IRouter} from "./IRouter"
+
+export class UserRouter implements IRouter {
+    private readonly router: Router
+
+    constructor() {
+        this.router = Router()
+        this.initRoutes()
+    }
+
+    public getRouter(): Router {
+        return this.router
+    }
+
+    private initRoutes(): void {
+        this.router.get('/', this.getUsers)
+    }
+
+    private getUsers(req: Request, res: Response): void {
+        console.log("getUsers")
+        res.send()
+    }
+}
