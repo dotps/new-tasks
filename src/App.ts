2feb1499bcdb111a0d6a14c6df7e554828aeae64
Stas Petrov <petrovstas@mail.ru>
2025-01-31 17:47:33 +0500

    TODO: внедрить prisma

--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,19 @@
 import express, {Application} from "express"
 import {UserRouter} from "./Routes/UserRouter"
 import {IRouter} from "./Routes/IRouter"
+import {IORM} from "./IORM"
+import { PrismaClient } from "@prisma/client"
 
 export class App {
     private app: Application
     private userRouter: IRouter
+    private orm: IORM
 
+        // TODO: внедрить prisma
     constructor() {
         this.app = express()
         this.app.use(express.json())
+        this.orm = new PrismaClient()
         this.userRouter = new UserRouter()
         this.initRoutes()
     }
