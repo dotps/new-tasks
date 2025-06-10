213568d6f3cb3da11f2e16f43c3d937744bb1525
Stas Petrov <petrovstas@mail.ru>
2025-06-10 17:52:19 +0500

    fix: use reduce for calculateWorkingTime #1

--- src/helpers/task-helper.ts
+++ src/helpers/task-helper.ts
@@ -2,13 +2,17 @@ import {TaskData} from "../data/types"
 
 export class TaskHelper {
     static calculateWorkingTime(tasks: Partial<TaskData>[]): number {
-        let seconds = 0
-        for (const task of tasks) {
-            if (task.createdAt && task.completedAt) {
-                const elapsedSeconds = (task.completedAt.getTime() - task.createdAt.getTime()) / 1000
-                seconds += elapsedSeconds
-            }
+        return tasks.reduce(this.sumTaskWorkingTime, START_FROM_SECOND)
+    }
+
+    static sumTaskWorkingTime(seconds: number, task: Partial<TaskData>): number {
+        if (task.createdAt && task.completedAt) {
+            const elapsedSeconds = (task.completedAt.getTime() - task.createdAt.getTime()) / MILLISECONDS_IN_SECOND
+            return seconds + elapsedSeconds
         }
         return seconds
     }
 }
+
+const MILLISECONDS_IN_SECOND = 1000
+const START_FROM_SECOND = 0
